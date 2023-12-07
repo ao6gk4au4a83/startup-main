a765c92de4bf9e3b5ed2d6b4f36a4f86ed6a9b9e
Jo_Jo_from_Mars <joshuayang99@gmail.com>
2023-12-07 16:42:32 -0700

    123

--- dist/simplelensert.js
+++
@@ -1,9 +0,0 @@
-const { MongoClient } = require('mongodb');
-
-const userName = 'holowaychuk';
-const password = 'express';
-const hostname = 'mongodb.com';
-
-const url = `mongodb+srv://cs260:cs260password@cluster0.w01aemq.mongodb.net/bnb`;
-
-const client = new MongoClient(url);
